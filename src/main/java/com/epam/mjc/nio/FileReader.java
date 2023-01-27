de0d56be12cd358b48adf7a340c20ce27c333562
Aziz1019 <starinc1998@gmail.com>
2023-01-27 16:15:37 +0500

    Initial Commit

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,29 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        try (BufferedReader br = new BufferedReader(new java.io.FileReader(file))) {
+            String st;
+            StringBuilder data = new StringBuilder();
+            while ((st = br.readLine()) != null) {
+                data.append(st).append(" ");
+            }
+            String[] dataArr = data.toString().split(" ");
+            profile.setName(dataArr[1]);
+            profile.setAge(Integer.parseInt(dataArr[3]));
+            profile.setEmail(dataArr[5]);
+            profile.setPhone(Long.parseLong(dataArr[7]));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return profile;
     }
 }
