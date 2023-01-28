707bc9ba898b74dc49a6496e49949ec2d7576c30
Aziz1019 <starinc1998@gmail.com>
2023-01-28 09:57:12 +0500

    Minor changes

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,29 +1,38 @@
 package com.epam.mjc.nio;
 
-import java.io.BufferedReader;
 import java.io.File;
-
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
         Profile profile = new Profile();
-        try (BufferedReader br = new BufferedReader(new java.io.FileReader(file))) {
-            String st;
-            StringBuilder data = new StringBuilder();
-            while ((st = br.readLine()) != null) {
-                data.append(st).append(" ");
+
+        try (RandomAccessFile randomAccessFile = new RandomAccessFile(file, "r");
+             FileChannel fileChannel = randomAccessFile.getChannel();) {
+            StringBuilder strData = new StringBuilder();
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+            while (fileChannel.read(buffer) > 0) {
+                buffer.flip();
+                while (buffer.hasRemaining()) {
+                    strData.append((char) buffer.get());
+                }
+                buffer.clear();
+                strData.append(" ");
             }
-            String[] dataArr = data.toString().split(" ");
-            profile.setName(dataArr[1]);
-            profile.setAge(Integer.parseInt(dataArr[3]));
-            profile.setEmail(dataArr[5]);
-            profile.setPhone(Long.parseLong(dataArr[7]));
+            String[] dataArr = strData.toString().split(" ");
+
+            profile.setName(dataArr[1].split("\n")[0].trim());
+            profile.setAge(Integer.valueOf(dataArr[2].split("\n")[0].trim()));
+            profile.setEmail(dataArr[3].split("\n")[0].trim());
+            profile.setPhone(Long.valueOf(dataArr[4].split("\n")[0].trim()));
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         return profile;
     }
 }
